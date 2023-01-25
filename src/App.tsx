ce47b8d7ee710468fb75fa0b862b64afbc8b7d9b
HsiaooooooJu <kelllllyko@gmail.com>
2023-01-25 22:07:53 +0800

    feat: add init hangman logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,40 @@
 import './App.css'
+import { useState } from 'react'
+import words from './wordList.json'
+import HangmanDrawing from './HangmanDrawing'
+import HangmanWord from './HangmanWord'
+import Keyboard from './Keyboard'
 
 export default function App() {
-  return <h1>Hello world</h1>
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    // Math.random(): return 0 <= random number < 1
+    // Math.floor(Math.random() * 5); // random number from 0 to 4
+    return words[Math.floor(Math.random() * words.length)]
+  })
+  // the state will be an array of string, string length will be 1
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+  return (
+    <div
+      style={{
+        maxWidth: '800px',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '2rem',
+        margin: '0 auto',
+        alignItems: 'center'
+      }}
+    >
+      <div
+        style={{
+          fontSize: '2rem',
+          textAlign: 'center'
+        }}
+      >
+        Lose / Win
+      </div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
+    </div>
+  )
 }
